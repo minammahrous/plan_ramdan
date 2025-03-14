0e083708191fd7c4ad1bf10604cdb9de6993930a
minammahrous <126823445+minammahrous@users.noreply.github.com>
2025-03-14 11:46:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.title("Production Scheduling App")
 
 st.markdown("Go to the [Demand Planning Page](./pages/Demand.py)")
+# Set the branch to "ramdan"
+st.session_state["branch"] = "ramdan"
